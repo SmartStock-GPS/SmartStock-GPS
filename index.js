8bd9e1e7b20dcb452893b5bc542182f810643fa1
Ath1000 <athkarandikar@gmail.com>
2023-06-14 20:13:51 +0530

    added add_stock.html, organized the public dir

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const favicon = require("serve-favicon");
-const app = express();
-const port = 3030;
+const express = require("express")
+const path = require("path")
+const bodyParser = require("body-parser")
+const favicon = require("serve-favicon")
+const app = express()
+const port = 3030
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
-app.use(favicon(path.join(__dirname, "public/img/logo-name.png")));
+app.use(favicon(path.join(__dirname, "public/assets/logos/logo-name.png")))
 
-app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
-app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+)
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+)
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/login.html"));
-});
+  res.sendFile(path.join(__dirname, "views/login.html"))
+})
+
+app.get("/add-stock", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/add_stock.html"))
+})
 
 app.get("/dashboard", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/dashboard.html"));
-});
+  res.sendFile(path.join(__dirname, "views/dashboard.html"))
+})
 
 app.listen(port, () => {
-    console.log(`SmartStock GPS listening on http://localhost:${port}`)
-});
+  console.log(`SmartStock GPS listening on http://localhost:${port}`)
+})
