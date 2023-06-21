56a140ce84b8255fc9f5fb237839371c97ca8869
Sharaneshwar Bharat Punjal <punjalsharaneshwar@gmail.com>
2023-06-21 09:33:43 +0530

    stock edit, increment, decrement done

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,12 +1,11 @@
+let update_id = '';
+
 $(document).ready(async (event) => {
     await fetch('/select_stock', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-        },
-        // body: JSON.stringify({
-        //     parameter: ''          
-        // })
+        }
     })
         .then((res) => res.json())
         .then(async function (res) {
@@ -19,8 +18,8 @@ $(document).ready(async (event) => {
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item d-md-flex d-grid">
                                 <div class="flex-fill">
-                                    <span>Quantity:</span>
-                                    <span>${element.quantity}</span>
+                                    <span>Quantity:</span> 
+                                    <span id="q-${element._id}">${element.quantity}</span>
                                 </div>
                                 <div class="flex-fill">
                                     <span>Price per item:</span>
@@ -31,21 +30,41 @@ $(document).ready(async (event) => {
                                     <span>Rs. ${element.quantity * element.price}</span>
                                 </div>
                                 <div class="flex-fill icons-container">
-                                    <icon class="material-icons">edit_note</icon>
-                                    <icon class="material-icons">remove_circle</icon>
-                                    <icon class="material-icons">add_circle</icon>
+                                    <button class="material-icons" onclick="edit('${element.name}')">edit_note</button>
+                                    <div class="btn-box">
+                                        <input type="text" id="add-${element._id}" maxlength="3">
+                                        <button class="material-icons" onclick="update('${element._id}',true)">add_circle</button>
+                                    </div>
+                                    <div class="btn-box">
+                                        <input type="text" id="rem-${element._id}" maxlength="3">
+                                        <button class="material-icons" onclick="update('${element._id}',false)">remove_circle</button>
+                                    </div>
                                 </div>
                             </li>
                         </ul>
                     </div>                
                 `)
+                $('.btn-box input').keypress(async function (event) {
+                    var keycode = event.which;
+                    if (keycode < 48 || keycode > 57)
+                        event.preventDefault();
+                });
             });
         })
 
     $("#preloader").css("display", "none");
 })
 
-$('#add-stock').click(async (event) => {
+$('#btnCollapse').click((event) => {
+    $("#add-stock").html("Add");
+    $("#quantity").prop('disabled', false);
+    $('#name').val('');
+    $('#quantity').val('');
+    $('#price-per-item').val('');
+    $('#total-price').val('');
+})
+
+$('#add-stock').click(async function (event) {
     let name = $('#name').val();
     let quantity = $('#quantity').val();
     let price = $('#price-per-item').val();
@@ -58,30 +77,27 @@ $('#add-stock').click(async (event) => {
             text: 'Please fill all the fields!',
         })
     } else {
-        const date = new Date()
-        const formattedDate = new Intl.DateTimeFormat('en-US', { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true }).format(date)
-
-        await fetch('/add_stock', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: name,
-                quantity: quantity,
-                price: price,
-                last_updated: formattedDate
+        if ($(this).html() == "Add") {
+            await fetch('/add_stock', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: name,
+                    quantity: quantity,
+                    price: price
+                })
             })
-        })
-            .then((res) => res.json())
-            .then(async function (res) {
-                if (res.status) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Success',
-                        text: 'Stock added successfully!',
-                    })
-                    $('#stock-area').append(`
+                .then((res) => res.json())
+                .then(async function (res) {
+                    if (res.status) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Stock added successfully!',
+                        })
+                        $('#stock-area').append(`
                         <div class="card">
                             <div class="card-header">
                                 ${$('#name').val()}
@@ -98,33 +114,153 @@ $('#add-stock').click(async (event) => {
                                     </div>
                                     <div class="flex-fill">
                                         <span>Total Price:</span>
-                                        <span>Rs. ${ $('#total-price').val()}</span>
+                                        <span>Rs. ${$('#total-price').val()}</span>
                                     </div>
                                     <div class="flex-fill icons-container">
-                                        <icon class="material-icons">edit_note</icon>
-                                        <icon class="material-icons">remove_circle</icon>
-                                        <icon class="material-icons">add_circle</icon>
+                                        <button class="material-icons" onclick="edit('${$('#name').val()}')">edit_note</button>
+                                        <div class="btn-box">
+                                            <input type="text" maxlength="3">
+                                            <button class="material-icons">add_circle</button>
+                                        </div>
+                                        <div class="btn-box">
+                                            <input type="text" maxlength="3">
+                                            <button class="material-icons">remove_circle</button>
+                                        </div>
                                     </div>
                                 </li>
                             </ul>
-                        </div>                
-                    `)
-
-                    $('#name').val('');
-                    $('#quantity').val('');
-                    $('#price-per-item').val('');
-                    $('#total-price').val('');
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'Something went wrong!',
-                    })
-                }
+                        </div>`);
+                        $('#name').val('');
+                        $('#quantity').val('');
+                        $('#price-per-item').val('');
+                        $('#total-price').val('');
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Something went wrong!',
+                        })
+                    }
+                })
+        } else {
+            await fetch('/update_stock', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: update_id,
+                    name: name,
+                    quantity: quantity,
+                    price: price
+                })
             })
+                .then((res) => res.json())
+                .then(async function (res) {
+                    if (res.status) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Stock updated successfully!',
+                        }).then(() => window.location.reload())
+                    }
+                })
+        }
     }
 })
 
 $('#quantity, #price-per-item').on('input', async (event) => {
     $('#total-price').val($('#quantity').val() * $('#price-per-item').val());
 })
+
+async function edit(item_name) {
+    $("#collapseExample").collapse("show");
+    $("#add-stock").html("Update");
+    $("#quantity").prop('disabled', true);
+
+    await fetch('/select_stock_from_name', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: item_name
+        })
+    })
+        .then((res) => res.json())
+        .then(async function (res) {
+            update_id = res._id;
+            $('#name').val(item_name);
+            $('#quantity').val(res.quantity);
+            $('#price-per-item').val(res.price);
+            $('#total-price').val(res.quantity * res.price);
+        })
+}
+
+async function update(_id, incre) {
+    if (incre) {
+        if ($('#add-' + _id).val() == '')
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please enter quantity to update!',
+            })
+        else {
+            await fetch('/increment_stock', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: _id,
+                    quantity: $('#add-' + _id).val()
+                })
+            })
+                .then((res) => res.json())
+                .then(async function (res) {
+                    if (res.status) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Stock incremented successfully!',
+                        }).then(() => window.location.reload())
+                    }
+                })
+        }
+    } else {
+        if ($('#rem-' + _id).val() == '')
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please enter quantity to update!',
+            })
+        else if (parseInt($('#rem-' + _id).val()) > parseInt($('#q-' + _id).html())) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Insufficient Stock!',
+            })
+        } else {
+            await fetch('/decrement_stock', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: _id,
+                    quantity: $('#rem-' + _id).val()
+                })
+            })
+                .then((res) => res.json())
+                .then(async function (res) {
+                    if (res.status) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Stock decremented successfully!',
+                        }).then(() => window.location.reload())
+                    }
+                })
+        }
+    }
+}
--- routes/dashboard_router.js
+++ routes/dashboard_router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require("path");
 const client = require('../dbconnect');
+const ObjectId = require('mongodb').ObjectId;
 const router = express.Router();
 
 router.get("", (req, res) => {
@@ -8,20 +9,75 @@ router.get("", (req, res) => {
 });
 
 router.post("/select_stock", async (req, res) => {
-    let items = await client.db("smartstock-db").collection("stock-items").find().toArray();
+    let items = await client.db("smartstock-db").collection("stock-items").find().sort({ last_updated: -1 }).toArray();
     res.json(items);
 });
 
 router.post("/add_stock", async (req, res) => {
     await client.db("smartstock-db").collection("stock-items").insertOne({
         name: req.body.name,
-        quantity: req.body.quantity,
+        quantity: parseInt(req.body.quantity),
         price: req.body.price,
-        last_updated: req.body.last_updated
+        last_updated: new Date()
     })
         .then(() => res.json({ status: true }))
         .catch(() => res.json({ status: false }))
 })
 
+router.post("/select_stock_from_name", async (req, res) => {
+    let items = await client.db("smartstock-db").collection("stock-items").find({ name: req.body.name }).toArray();
+    if (items.length != 0)
+        res.json(items[0]);
+})
+
+router.post("/update_stock", async (req, res) => {
+    await client.db("smartstock-db").collection("stock-items").updateOne(
+        {
+            _id: new ObjectId(req.body.id)
+        },
+        {
+            $set: {
+                name: req.body.name,
+                price: req.body.price,
+                last_updated: new Date()
+            }
+        })
+        .then(() => res.json({ status: true }))
+        .catch(() => res.json({ status: false }))
+})
+
+router.post('/increment_stock', async (req, res) => {
+    await client.db("smartstock-db").collection("stock-items").updateOne(
+        {
+            _id: new ObjectId(req.body.id)
+        },
+        {
+            $inc: {
+                quantity: parseInt(req.body.quantity)
+            },
+            $set: {
+                last_updated: new Date()
+            }
+        })
+        .then(() => res.json({ status: true }))
+        .catch(() => res.json({ status: false }))
+})
+
+router.post('/decrement_stock', async (req, res) => {
+    await client.db("smartstock-db").collection("stock-items").updateOne(
+        {
+            _id: new ObjectId(req.body.id)
+        },
+        {
+            $inc: {
+                quantity: parseInt(req.body.quantity) * -1
+            },
+            $set: {
+                last_sold: new Date()
+            }
+        })
+        .then(() => res.json({ status: true }))
+        .catch(() => res.json({ status: false }))
+})
 
 module.exports = router
