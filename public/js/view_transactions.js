1091ee05c1fb9405929095f44386074438b640d8
Sharaneshwar Bharat Punjal <punjalsharaneshwar@gmail.com>
2023-07-04 11:58:24 +0530

    view transactions fetching and searching done

--- public/js/view_transactions.js
+++ public/js/view_transactions.js
@@ -1,4 +1,63 @@
 $(document).ready(async (event) => {
+    await fetch('/view-transactions/select_transactions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.status) {
+                for (let i = 0; i < res.dates.length; i++) {
+                    if (i == 0 || (i != 0 && res.dates[i].split(" ")[0] != res.dates[i - 1].split(" ")[0])) {
+                        $("#t_container").append(`
+                        <div class="card month-card">
+                            <div class="card-header text-bg-secondary">
+                                <span>${res.dates[i].split(" ")[0].toUpperCase()} MONTH SALE</span>
+                                <span>Rs. ${res.monthly_sales[res.dates[i].split(" ")[0]]}</span>
+                            </div>
+                        </div>
+                        `);
+                    }
+
+                    $("#t_container").append(`
+                        <div class="card" id="${i}">
+                        	<div class="card-header">
+                        		<span>${res.dates[i]}</span>
+                        		<span class="float-end">Rs. ${res.total_sales[i]}</span>
+                        	</div>
+                        	<ul class="list-group list-group-flush"></ul>
+                        </div>
+                    `);
+
+                    res.transactions[res.dates[i]].forEach((transaction) => {
+                        $(`#${i} ul`).append(`
+                            <li class="list-group-item d-md-flex d-grid">
+                                <div class="flex-even">
+                                    <span>${transaction.name}</span>
+                                </div>
+                                <div class="flex-even">
+                                    <span>Quantity:</span>
+                                    <span>${transaction.quantity}</span>
+                                </div>
+                                <div class="flex-even">
+                                    <span>SP/Item:</span>
+                                    <span>Rs. ${transaction.selling_price}</span>
+                                </div>
+                                <div class="flex-even">
+                                    <span>Total Price:</span>
+                                    <span>Rs. ${transaction.total}</span>
+                                </div>
+                            </li>
+                        `);
+                    })
+                }
+                $('.card .list-group-item').css('display', 'none');
+            } else {
+                $("#t_container").append("<h5>No transactions found</h5>");
+            }
+        })
+
     $("#preloader").css("display", "none");
 })
 
@@ -20,6 +79,25 @@ $(".nav-item:last a").click(async function (event) {
             })
                 .then((res) => res.json())
                 .then((res) => window.location.replace('/'))
+        }
+    })
+})
+
+$('#search').on('input', async (event) => {
+    query = $('#search').val().toLowerCase();
+    $('.card .list-group-item').each(function () {
+        if ($(this).find('.flex-even').eq(0).find('span').html().toLowerCase().includes(query)) {
+            $(this).removeClass('importantRule');
+        } else {
+            $(this).addClass('importantRule');
+        }
+    })
+
+    $('.card').each(function () {
+        if ($(this).find('.list-group-item').hasClass('importantRule') && $(this).find('.list-group-item').length == $(this).find('.importantRule').length) {
+            $(this).addClass('importantRule');
+        } else {
+            $(this).removeClass('importantRule');
         }
     })
 })
--- routes/view_transactions_router.js
+++ routes/view_transactions_router.js
@@ -16,4 +16,61 @@ router.post('/logout', async (req, res) => {
     res.json({ status: true })
 })
 
+router.post('/select_transactions', async (req, res) => {
+    let transactions = await client.db("smartstock-db").collection("transactions").find().sort({ date_sold: -1 }).toArray();
+    if (transactions.length == 0)
+        res.json({ status: false })
+    else {
+        let result = {}
+        let dates = new Array()
+        let total_sales = new Array()
+        let monthly_sales = {}
+
+        // Grouping transactions by date
+        transactions.forEach((transaction) => {
+            let date = transaction.date_sold.toLocaleDateString('en-US', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric'
+            });
+            if (result[date] == undefined) {
+                dates.push(date)
+                result[date] = []
+            }
+            result[date].push({
+                _id: transaction._id,
+                name: transaction.name,
+                quantity: transaction.quantity,
+                selling_price: transaction.selling_price,
+                total: transaction.selling_price * transaction.quantity
+            })
+        })
+
+        // Calculating each day total sale
+        let month_total = 0
+        for (let i = 0; i < dates.length; i++) {
+            let day_total = 0
+            result[dates[i]].forEach((transaction) => {
+                day_total += transaction.total
+            })
+            total_sales.push(day_total)
+            month_total += day_total
+            if (i != dates.length - 1 && dates[i].split(" ")[0] != dates[i + 1].split(" ")[0]) {
+                monthly_sales[dates[i].split(" ")[0]] = month_total
+                month_total = 0
+            }
+        }
+        monthly_sales[dates[dates.length - 1].split(" ")[0]] = month_total
+        console.log(monthly_sales);
+
+        res.json({
+            status: true,
+            dates: dates,
+            transactions: result,
+            total_sales: total_sales,
+            monthly_sales: monthly_sales
+        })
+    }
+})
+
 module.exports = router
